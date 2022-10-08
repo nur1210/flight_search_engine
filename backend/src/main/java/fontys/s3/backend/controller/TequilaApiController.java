9ed7197157ffd98201b2e55daef734d27cf1aa72
Nur Nechushtan <495554@student.fontys.nl>
2022-10-08 13:14:03 +0200

    Add 2 get mapping for the tequila controller

--- backend/src/main/java/fontys/s3/backend/controller/TequilaApiController.java
+++ backend/src/main/java/fontys/s3/backend/controller/TequilaApiController.java
@@ -1,8 +1,9 @@
 package fontys.s3.backend.controller;
 
+import fontys.s3.backend.business.GetAirportByCityUseCase;
+import fontys.s3.backend.business.GetAirportByCordsUseCase;
 import fontys.s3.backend.business.GetAllFlightsFromOriginToDestinationUseCase;
-import fontys.s3.backend.domain.GetAllFlightsFromOriginToDestinationRequest;
-import fontys.s3.backend.domain.GetAllFlightsFromOriginToDestinationResponse;
+import fontys.s3.backend.domain.*;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,22 +14,24 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class TequilaApiController {
     private final GetAllFlightsFromOriginToDestinationUseCase getAllFlightsFromOriginToDestinationUseCase;
+    private final GetAirportByCityUseCase getAirportByCityUseCase;
+    private final GetAirportByCordsUseCase getAirportByCordsUseCase;
 
 
     @GetMapping("/flights")
     public ResponseEntity<GetAllFlightsFromOriginToDestinationResponse> getAllFlightsFromOriginToDestination(@RequestParam(value = "fly_from", required = false) String flyFrom,
-                                                                                      @RequestParam(value = "fly_to", required = false) String flyTo,
-                                                                                      @RequestParam(value = "date_from", required = false) String dateFrom,
-                                                                                      @RequestParam(value = "date_to", required = false) String dateTo,
-                                                                                      @RequestParam(value = "return_from", required = false) String returnFrom,
-                                                                                      @RequestParam(value = "return_to", required = false) String returnTo,
-                                                                                      @RequestParam(value = "flight_type", required = false) String flightType,
-                                                                                      @RequestParam(value = "adults", required = false) long adults,
-                                                                                      @RequestParam(value = "selected_cabins", required = false) String selectedCabins,
-                                                                                      @RequestParam(value = "curr", required = false) String curr,
-                                                                                      @RequestParam(value = "locale", required = false) String locale,
-                                                                                      @RequestParam(value = "max_stopovers", required = false) long maxStopovers,
-                                                                                      @RequestParam(value = "max_sector_stopovers", required = false) long maxSectorStopovers) {
+                                                                                                             @RequestParam(value = "fly_to", required = false) String flyTo,
+                                                                                                             @RequestParam(value = "date_from", required = false) String dateFrom,
+                                                                                                             @RequestParam(value = "date_to", required = false) String dateTo,
+                                                                                                             @RequestParam(value = "return_from", required = false) String returnFrom,
+                                                                                                             @RequestParam(value = "return_to", required = false) String returnTo,
+                                                                                                             @RequestParam(value = "flight_type", required = false) String flightType,
+                                                                                                             @RequestParam(value = "adults", required = false) long adults,
+                                                                                                             @RequestParam(value = "selected_cabins", required = false) String selectedCabins,
+                                                                                                             @RequestParam(value = "curr", required = false) String curr,
+                                                                                                             @RequestParam(value = "locale", required = false) String locale,
+                                                                                                             @RequestParam(value = "max_stopovers", required = false) long maxStopovers,
+                                                                                                             @RequestParam(value = "max_sector_stopovers", required = false) long maxSectorStopovers) {
         GetAllFlightsFromOriginToDestinationRequest request = GetAllFlightsFromOriginToDestinationRequest.builder()
                 .fly_from(flyFrom)
                 .fly_to(flyTo)
@@ -46,7 +49,25 @@ public class TequilaApiController {
                 .build();
         GetAllFlightsFromOriginToDestinationResponse response = getAllFlightsFromOriginToDestinationUseCase.getAllFlightsFromOriginToDestination(request);
         return ResponseEntity.ok(response);
+    }
 
+    @GetMapping("/locations/city")
+    public ResponseEntity<GetAirportResponse> getAirportByCity(@RequestParam(value = "city", required = false) String city) {
+        GetAirportByCityRequest request = GetAirportByCityRequest.builder()
+                .city(city)
+                .build();
+        GetAirportResponse response = getAirportByCityUseCase.getAirportByCity(request);
+        return ResponseEntity.ok(response);
+    }
 
+    @GetMapping("/locations/cords")
+    public ResponseEntity<GetAirportResponse> getAirportByCity(@RequestParam(value = "lat", required = false) String lat,
+                                                              @RequestParam(value = "lng", required = false) String lng) {
+        GetAirportByCordsRequest request = GetAirportByCordsRequest.builder()
+                .latitude(lat)
+                .longitude(lng)
+                .build();
+        GetAirportResponse response = getAirportByCordsUseCase.getAirportByCords(request);
+        return ResponseEntity.ok(response);
     }
 }
