6628bf225e37b5079ec3002777857c2d38436cee
Nur Nechushtan <495554@student.fontys.nl>
2022-10-11 11:57:51 +0200

    a

--- frontend/src/components/FlightsList.js
+++ frontend/src/components/FlightsList.js
@@ -3,33 +3,34 @@ import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "
 
 
 const FlightsList = ({flights}) => {
-    if (!flights) {
+    if (flights.length === 0) {
         return null;
+    } else {
+        return (
+            <div className={"justify-content-center align-content-center"}>
+                <TableContainer>
+                    <Table sx={{minWidth: 650}} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align={"center"}>Outbound</TableCell>
+                                {flights[0].route.length > 1 ? <TableCell align={"center"}>Return</TableCell> : null}
+                                <TableCell align={"center"}>Price</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                flights.map((flight) => {
+                                    console.log(flight);
+                                    return <BasicTable key={flight.id} route={flight.route} price={flight.price}
+                                                       link={flight.deepLink}/>
+                                })
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        );
     }
-
-    return (
-        <div className={"justify-content-center align-content-center"}>
-            <TableContainer>
-                <Table sx={{minWidth: 650}} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align={"center"}>Outbound</TableCell>
-                            {flights[0].route.length > 1 ? <TableCell align={"center"}>Return</TableCell> : null}
-                            <TableCell align={"center"}>Price</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            flights.map((flight) => {
-                                console.log(flight);
-                                return <BasicTable key={flight.id} route={flight.route} price={flight.price} link={flight.deepLink}/>
-                            })
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    );
 }
 
 export default FlightsList;
